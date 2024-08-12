f0a4057094e981f31c84f1016f2fac834144430f
BigZetena <V.O.Sharipov@gmail.com>
2024-08-12 18:51:06 +0300

    change back

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = 4321;
+const port = 3000;
 app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
